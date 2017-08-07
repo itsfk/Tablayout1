387460342e99bf5e1fbe525dc956c22dfc182a1b
itsfk <faiz.ggmu@gmail.com>
2017-08-08 01:30:59 +0500

    Initial commit

---
+++ app/src/main/java/com/example/faiz/tablayout1/Fragment2.java
@@ -0,0 +1,33 @@
+package com.example.faiz.tablayout1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Faiz on 8/8/2017.
+ */
+
+public class Fragment2 extends Fragment {
+    public Fragment2() {
+
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+
+
+
+}
